d7556724a457935bca430f127f073e22c162d6ae
Patryk Walaszkowski <pwalaszkowski@gmail.com>
2025-01-16 23:26:06 +0100

    initial_commit_simple_tennis_club_application

---
+++ club/src/main/java/com/tennisclub/club/SimpleTennisClubApplication.java
@@ -0,0 +1,13 @@
+package com.tennisclub.club;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTennisClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleTennisClubApplication.class, args);
+	}
+
+}
---
+++ club/src/test/java/com/tennisclub/club/SimpleTennisClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tennisclub.club;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTennisClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
